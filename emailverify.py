9f352dddcaeb0a360ea26da0cbb6de3222758c5b
proweb7 <45687254+proweb7@users.noreply.github.com>
2023-07-03 12:54:31 +0100

    Update emailverify.py

--- emailverify.py
+++ emailverify.py
@@ -1,63 +1,80 @@
-#!/usr/bin/python
-# _*_ coding:utf-8 _*_
+#!/usr/bin/python  
+# _*_ coding:utf-8 _*_  
 
+import requests  
+import datetime  
+import pycurl  
 
-import requests
-import datetime
+class BouncelessOne():  
+    """
+    Class for individual email verification.
+    """
+    def __init__(self, key, email):  
+        self.key = key  
+        self.email = email  
+        self.verif = "https://apps.bounceless.io/api/singlemaildetails?secret="  
+        self.url = f"{self.verif}{self.key}&email={self.email}"  
 
+    def control(self):  
+        """
+        Verifies the email and returns a JSON response.
+        """
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+        except requests.HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+        else:
+            return response.json()  # returns a JSON response  
 
-class bouncelessOne():
+class BouncelessBulk():  
+    """
+    Class for bulk email verification.
+    """
+    def __init__(self, key, user_file):  
+        datenow = datetime.datetime.now()  
+        self.key = key  
+        self.name = f'File{datenow.strftime("%Y-%m-%d %H:%M")}'
+        self.user_file = user_file  
+        self.url = f'https://apps.bounceless.io/api/verifyApiFile?secret={key}&filename={self.name}'
 
-    def __init__(self, key, email):
-        self.key = key
-        self.email = email
-        self.verif = "https://apps.bounceless.io/api/verifyEmail?secret="
-        self.url = self.verif+self.key+"&email="+self.email
+    def upload(self):  
+        """
+        Uploads the file for verification.
+        """
+        with open('id_file', 'w') as infile:  
+            c = pycurl.Curl()  
+            c.setopt(c.POST, 1)  
+            c.setopt(c.URL, self.url)  
+            c.setopt(c.HTTPPOST, [('file_contents', (  
+                c.FORM_FILE, self.user_file,  
+                c.FORM_CONTENTTYPE, 'text/plain',  
+                c.FORM_FILENAME, self.name.replace(' ','_'),))])  
+            c.setopt(c.WRITEFUNCTION, infile.write)  
+            c.setopt(c.VERBOSE, 1)  
+            c.perform()  
+            c.close()  
 
+    def get_info(self):  
+        """
+        Gets the verification results.
+        """
+        with open('id_file','r') as f:  
+            ids = f.read()  
+            url = f'https://apps.bounceless.io/api/getApiFileInfo?secret={self.key}&id={ids}'
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+            except requests.HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')
+            except Exception as err:
+                print(f'Other error occurred: {err}')
+            else:
+                with open('result.txt', 'a') as res:  
+                    res.write(response.content+'\n')  
+                print(response.content)
 
-    def control(self):
-        r = requests.get(self.url)
-        return r.text
-
-
-class bouncelessBulk():
-
-    def __init__(self, key, user_file):
-        datenow = datetime.datetime.now()
-        self.key = key
-        self.name = 'File' + datenow.strftime("%Y-%m-%d %H:%M")
-        self.user_file = user_file
-        self.url = 'https://apps.bounceless.io/api/verifyApiFile?secret='+key+'&filename=%s' % self.name
-
-
-    def upload(self):
-        import pycurl
-
-        infile = open('id_file', 'w')
-        c = pycurl.Curl()
-        c.setopt(c.POST, 1)
-        c.setopt(c.URL, self.url)
-        c.setopt(c.HTTPPOST, [('file_contents', (
-                    c.FORM_FILE, self.user_file,
-                    c.FORM_CONTENTTYPE, 'text/plain',
-                    c.FORM_FILENAME, self.name.replace(' ','_'),)),])
-        c.setopt(c.WRITEFUNCTION, infile.write)
-        c.setopt(c.VERBOSE, 1)
-        c.perform()
-        c.close()
-
-
-    def get_info(self):
-
-        with open('id_file','r') as f:
-            ids = f.read()
-        url = 'https://apps.bounceless.io/api/getApiFileInfo?secret='+self.key+'&id=%s' % ids
-        r = requests.get(url)
-        with open('result.txt', 'a') as res:
-            res.write(r.content+'\n')
-        print r.content
-
-
-if __name__ == '__main__':
+if __name__ == '__main__':  
     pass
-
